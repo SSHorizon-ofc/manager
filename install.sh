6c827087a7e7fe7239ce261a35b63aa321aef76b
SSHorizon-ofc <master@sshorizon.xyz>
2025-07-29 00:40:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 curl -s -L -o /usr/local/bin/sshorizon https://github.com/SSHorizon-ofc/manager/raw/refs/heads/main/$(uname -m)/sshorizon
 chmod +x /usr/local/bin/sshorizon
+mkdir /opt/sshorizon/ssl
 sshorizon
