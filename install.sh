8c9362a4bd9b335b13d0de41f1414abf1b34e77a
SSHorizon-ofc <master@sshorizon.xyz>
2025-08-08 16:08:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 curl -s -L -o /usr/local/bin/sshorizon https://github.com/SSHorizon-ofc/manager/raw/refs/heads/main/$(uname -m)/sshorizon
 chmod +x /usr/local/bin/sshorizon
-mkdir /opt/sshorizon/ssl
+mkdir -p /opt/sshorizon/ssl
+
+if ! grep -q '^MaxStartups 1000:10:1000$' /etc/ssh/sshd_config; then
+    echo 'MaxStartups 1000:10:1000' >> /etc/ssh/sshd_config
+    service ssh restart
+fi
+
 sshorizon
