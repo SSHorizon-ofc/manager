5ec3dacaafdbeb52acf80edc3d8af9defed37335
SSHorizon-ofc <master@sshorizon.xyz>
2025-11-01 14:24:21 -0300

    Modify install.sh for new SSHorizon installation
    Updated installation script to use new URL and added SSL certificate generation.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-curl -s -L -o /usr/local/bin/sshorizon https://github.com/SSHorizon-ofc/manager/raw/refs/heads/main/$(uname -m)/sshorizon
+#!/bin/bash
+
+curl -s -L -o /usr/local/bin/sshorizon https://install.sshorizon.shop/manager/$(uname -m)/install-sshorizon
 chmod +x /usr/local/bin/sshorizon
-mkdir -p /opt/sshorizon/ssl
+sudo mkdir -p /opt/sshorizon/ssl
+
+sudo openssl req -x509 -nodes -days 365 \
+  -newkey rsa:2048 \
+  -keyout /opt/sshorizon/ssl/privkey.pem \
+  -out /opt/sshorizon/ssl/fullchain.pem \
+  -subj "/C=BR/ST=SP/L=SaoPaulo/O=SSHorizon/OU=TI/CN=example.com"
 
 if ! grep -q '^MaxStartups 1000:10:1000$' /etc/ssh/sshd_config; then
     echo 'MaxStartups 1000:10:1000' >> /etc/ssh/sshd_config
